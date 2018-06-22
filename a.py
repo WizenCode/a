d5907d4b969e0f923138feb0302fc226df64c097
Wizen <33266596+WizenCode@users.noreply.github.com>
2018-06-22 08:47:25 -0700

    Update a.py

--- a.py
+++ a.py
@@ -154,8 +154,21 @@ def add(m):
     groups = str(redis.sismember("groups" , "{}".format(chatid)))
     if (userid in sudos or bot.get_chat_member(chatid , userid).status!="member") and chat=="supergroup":
         if groups=="True":
-            bot.send_message(chatid , """*⌥ PHOTO : {}
-*""".format(lockphoto(chatid)) , parse_mode="Markdown")
+            bot.send_message(chatid , """*➥All : `{}`
+➥Text : `{}`
+➥Photo : `{}`
+➥Sticker : `{}`
+➥Video : `{}`
+➥Audio : `{}`
+➥Gif : `{}`
+➥Game : `{}`
+➥TGServices : `{}`
+➥Link : `{}`
+➥Forward : `{}`
+➥VideoNote : `{}`
+➥Voice : `{}`
+➥Document : `{}`
+*""".format(lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid),lockphoto(chatid)) , parse_mode="Markdown")
     else:
         bot.send_message(chatid , "*⌥ You are not admin or chat is not a supergroup!*" , parse_mode="Markdown")
 #######################################################################################################################################################################
